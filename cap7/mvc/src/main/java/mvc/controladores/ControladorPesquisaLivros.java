47b5a03e2bd82c61d2b88c77a6c6f9f8f97de6dd
Cosmo Matias <90862829+ducosmo@users.noreply.github.com>
2026-03-01 22:48:03 -0300

    Update ControladorPesquisaLivros.java

--- cap7/mvc/src/main/java/mvc/controladores/ControladorPesquisaLivros.java
+++ cap7/mvc/src/main/java/mvc/controladores/ControladorPesquisaLivros.java
@@ -1,36 +1,42 @@
 package mvc.controladores;
 
-import static spark.Spark.*;
+import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
 import mvc.modelo.*;
 import mvc.visao.*;
 
 public class ControladorPesquisaLivros {
-  ServicoPesquisaLivros pesq;
-  PaginaDadosLivro pagina; 
+  private ServicoPesquisaLivros pesq;
+  private PaginaDadosLivro pagina; 
 
   public ControladorPesquisaLivros(ServicoPesquisaLivros pesq, PaginaDadosLivro pagina) {
     this.pesq = pesq;
     this.pagina = pagina;
-   }
+  }
   
   public void start() {
-    port(4567);
+    // Criação da instância do Javalin com arquivos estáticos
+    Javalin app = Javalin.create(config -> {
+        config.staticFiles.add("/static", Location.CLASSPATH);
+    }).start(4567);
   
-    staticFiles.location("/static");
-        
-    get("/", (req, res) -> { 
-       res.redirect("index.html");
-       return null;
-    });
+    // Rota Raiz
+    app.get("/", ctx -> ctx.redirect("/index.html"));
 
-    get("/pesquisa", (req, res) -> { 
-       String autor = req.queryParams("autor");
+    // Rota de Pesquisa
+    app.get("/pesquisa", ctx -> { 
+       String autor = ctx.queryParam("autor");
+       
        Livro livro = pesq.pesquisaPorAutor(autor);
-       return pagina.exibeLivro(livro.getTitulo(), livro.getAutor(), livro.getISBN());
+       
+       if (livro != null) {
+           String html = pagina.exibeLivro(livro.getTitulo(), livro.getAutor(), livro.getISBN());
+           ctx.html(html); // Define o content-type como text/html automaticamente
+       } else {
+           ctx.status(404).result("Livro não encontrado para o autor: " + autor);
+       }
     });
 
-    init();
-    awaitInitialization();
-    System.out.println("Spark rodando em http://localhost:" + port());
-   }
+    System.out.println("Javalin rodando em http://localhost:4567");
+  }
 }
